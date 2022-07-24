286b52ff35ac8a020746ca11c80b55dd39ca965c
vivekkushal <vivekjss1@gmail.com>
2022-07-25 01:38:20 +0530

    style cleanup

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -5,7 +5,9 @@ function Cart(props) {
     <form class="d-flex">
       <button class="btn btn-outline-dark" type="submit">
         <i class="bi bi-cart-fill me-1"></i>
+        <span> </span>
         Cart
+        <span> </span>
         <span class="badge bg-dark text-white ms-1 rounded-pill">
           {props.count}
         </span>
